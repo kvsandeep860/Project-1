7711956368a8a3fb9be2d1b3aa7c8cb4c34a9a45
Sandeep <sandeep@example.com>
2025-10-02 17:19:33 +0530

    Data Ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.datascience import logger
-print("logger got imported")
+from src.datascience.pipeline.data_ingestion import DataIngestionTrainingPipeline
 logger.info("logger is working fine")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f"Stage {STAGE_NAME} started")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
